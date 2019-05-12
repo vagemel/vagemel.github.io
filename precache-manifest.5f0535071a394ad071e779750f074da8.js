159fee9ffc5895f7065c7ca737158f18d10c72b7
Vage Melikbekyan <vage.m@papara.ru>
2019-05-12 13:50:57 +0300

    Initial commit

---
+++ precache-manifest.5f0535071a394ad071e779750f074da8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "92db96a03eced7980ffc",
+    "url": "/js/chunk-vendors.7a86dcb5.js"
+  },
+  {
+    "revision": "ddc34d9143bc1796f2f9",
+    "url": "/js/app.f89ecd4a.js"
+  },
+  {
+    "revision": "d5d4c8cf03f766384af60683c08e91d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddc34d9143bc1796f2f9",
+    "url": "/css/app.720811a7.css"
+  }
+];
